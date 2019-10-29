65898c6d8dc31f64978cc69312489cdea1a38c60
Timothy Oliver <oliverwakogi@live.com>
2019-10-29 11:28:17 +0300

    add comments for initial files

--- slackbot.py
+++ slackbot.py
@@ -2,13 +2,24 @@ from slackclient import SlackClient
 import json
 import time
 
+# the bot currently replies only to `BOT TEST`. this is a test feature to
+# ensure that the bot is working
+# features will be appended to this code base
+
+# configuration happens here :)
+# var:
+# 	slack_bot_token: contains user bot tokens
 tokens = {}
 with open('configs.json') as json_data:
 	tokens = json.load(json_data)
 
+# declare client instance
 slack_client = SlackClient(tokens.get('slack_bot_token'))
 
+# connect to client api
 if slack_client.rtm_connect():
+
+	#inifinite loop that continously checks for events
 	while slack_client.server.connected is True:
 		messages = slack_client.rtm_read()
 		print(messages)
@@ -19,7 +30,8 @@ if slack_client.rtm_connect():
 					channel = message['channel']
 					send_message = 'Responding to `BOT TEST` message sent by user <@%s>' % message['user']
 					slack_client.api_call('chat.postMessage', channel=channel, text=send_message)
-
+		
+		# ensures api makes a call per second
 		time.sleep(1)
 
 else:
