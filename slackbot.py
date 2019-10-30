61638799a8a392cfeead2a3faca5afdfcbd7aba7
Timothy Oliver <oliverwakogi@live.com>
2019-10-30 18:35:00 +0300

    add event handlers

--- slackbot.py
+++ slackbot.py
@@ -1,39 +1,80 @@
-from slackclient import SlackClient
-import json
-import time
+from slackeventsapi import SlackEventAdapter
+import slack
+import os
+from flask import Flask
 
-# the bot currently replies only to `BOT TEST`. this is a test feature to
-# ensure that the bot is working
-# features will be appended to this code base
+app = Flask(__name__)
+slack_events_adapter = SlackEventAdapter(os.environ.get('SLACK_SIGNING_SECRET'), '/slack/events', app)
+client =  slack.WebClient(token=os.environ.get('SLACK_BOT_TOKEN'))
 
-# configuration happens here :)
-# var:
-# 	slack_bot_token: contains user bot tokens
-tokens = {}
-with open('configs.json') as json_data:
-	tokens = json.load(json_data)
+@slack_events_adapter.on("message")
+def handle_message(event_data):
+    print('-' * 50)
+    print(event_data)
 
-# declare client instance
-slack_client = SlackClient(tokens.get('slack_bot_token'))
+    if 'subtype' in event_data["event"] and event_data["event"]["subtype"] == 'channel_join':\
+        client.chat_postMessage(
+			channel=event_data["event"]["channel"],
+			blocks=[
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": f"Hi, i am ava",
+                    }
+                }
+            ]
+		)
 
-# connect to client api
-if slack_client.rtm_connect():
+    message = event_data["event"]
+    if message.get("subtype") is None and "BOT TEST" in message.get('text'):
+        channel = message["channel"]
+        send_message = "Responding to `BOT TEST` message sent by user <@%s>" % message["user"]
+        client.chat_postMessage(channel=channel, text=send_message)
 
-	#inifinite loop that continously checks for events
-	while slack_client.server.connected is True:
-		messages = slack_client.rtm_read()
-		print(messages)
-		print('--' * 50)
+@slack_events_adapter.on('app_mention')
+def handle_mention(event_data):
+    print(event_data)
+    client.chat_postMessage(
+        channel=event_data["event"]["channel"],
+        blocks=[
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "Someone mentioned my name :smiley:"
+                }
+            }
+        ]
+    )
 
-		if messages:
-			for message in messages:
-				if message.get("subtype") is None and message.get('user') is not None and message.get('text') is not None and  "BOT TEST" in message.get('text'):
-					channel = message['channel']
-					send_message = 'Responding to `BOT TEST` message sent by user <@%s>' % message['user']
-					slack_client.api_call('chat.postMessage', channel=channel, text=send_message)
-		
-		# ensures api makes a call per second
-		time.sleep(1)
+@slack_events_adapter.on('im_created')
+def im_created(event_data):
+    print('x' * 20)
+    print('im_created triggered')
+    print(event_data)
+    print('x' * 20)
 
-else:
-	print('Connection failed')
+
+@slack_events_adapter.on('im_close')
+def im_close(event_data):
+    print('x' * 20)
+    print('im_close triggered')
+    print(event_data)
+    print('x' * 20)
+
+
+@slack_events_adapter.on('im_open')
+def im_open(event_data):
+    print('x' * 20)
+    print('im_open triggered')
+    print(event_data)
+    print('x' * 20)
+
+
+@slack_events_adapter.on("error")
+def error_handler(err):
+    print("ERROR: " + str(err))
+
+if __name__ == "__main__":
+    app.run(port=3000, debug=True)
